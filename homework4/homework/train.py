241394684e142cab64c8610aa19fac47c6864d06
braydendewitt <dewittbrayden@gmail.com>
2024-03-19 18:29:10 -0400

    commit

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -7,111 +7,7 @@ from . import dense_transforms
 import torch.utils.tensorboard as tb
 import torch.optim as optim
 
-## Taken from tests.py file for AP calculations ##
-def point_in_box(pred, lbl):
-    px, py = pred[:, None, 0], pred[:, None, 1]
-    x0, y0, x1, y1 = lbl[None, :, 0], lbl[None, :, 1], lbl[None, :, 2], lbl[None, :, 3]
-    return (x0 <= px) & (px < x1) & (y0 <= py) & (py < y1)
-
-
-def point_close(pred, lbl, d=5):
-    px, py = pred[:, None, 0], pred[:, None, 1]
-    x0, y0, x1, y1 = lbl[None, :, 0], lbl[None, :, 1], lbl[None, :, 2], lbl[None, :, 3]
-    return ((x0 + x1 - 1) / 2 - px) ** 2 + ((y0 + y1 - 1) / 2 - py) ** 2 < d ** 2
-
-
-def box_iou(pred, lbl, t=0.5):
-    px, py, pw2, ph2 = pred[:, None, 0], pred[:, None, 1], pred[:, None, 2], pred[:, None, 3]
-    px0, px1, py0, py1 = px - pw2, px + pw2, py - ph2, py + ph2
-    x0, y0, x1, y1 = lbl[None, :, 0], lbl[None, :, 1], lbl[None, :, 2], lbl[None, :, 3]
-    iou = (abs(torch.min(px1, x1) - torch.max(px0, x0)) * abs(torch.min(py1, y1) - torch.max(py0, y0))) / \
-          (abs(torch.max(px1, x1) - torch.min(px0, x0)) * abs(torch.max(py1, y1) - torch.min(py0, y0)))
-    return iou > t
-
-
-class PR:
-    def __init__(self, min_size=20, is_close=point_in_box):
-        self.min_size = min_size
-        self.total_det = 0
-        self.det = []
-        self.is_close = is_close
-
-    def add(self, d, lbl):
-        lbl = torch.as_tensor(lbl, dtype=torch.float32).view(-1, 4)
-        #lbl = torch.as_tensor(lbl.astype(float), dtype=torch.float32).view(-1, 4)
-        d = torch.as_tensor(d, dtype=torch.float32).view(-1, 5)
-        all_pair_is_close = self.is_close(d[:, 1:], lbl)
-
-        # Get the box size and filter out small objects
-        sz = abs(lbl[:, 2]-lbl[:, 0]) * abs(lbl[:, 3]-lbl[:, 1])
-
-        # If we have detections find all true positives and count of the rest as false positives
-        if len(d):
-            detection_used = torch.zeros(len(d))
-            # For all large objects
-            for i in range(len(lbl)):
-                if sz[i] >= self.min_size:
-                    # Find a true positive
-                    s, j = (d[:, 0] - 1e10 * detection_used - 1e10 * ~all_pair_is_close[:, i]).max(dim=0)
-                    if not detection_used[j] and all_pair_is_close[j, i]:
-                        detection_used[j] = 1
-                        self.det.append((float(s), 1))
-
-            # Mark any detection with a close small ground truth as used (no not count false positives)
-            detection_used += all_pair_is_close[:, sz < self.min_size].any(dim=1)
-
-            # All other detections are false positives
-            for s in d[detection_used == 0, 0]:
-                self.det.append((float(s), 0))
-
-        # Total number of detections, used to count false negatives
-        self.total_det += int(torch.sum(sz >= self.min_size))
-
-
-    @property
-    def curve(self):
-        true_pos, false_pos = 0, 0
-        r = []
-        for t, m in sorted(self.det, reverse=True):
-            if m:
-                true_pos += 1
-            else:
-                false_pos += 1
-            prec = true_pos / (true_pos + false_pos)
-            recall = true_pos / self.total_det
-            r.append((prec, recall))
-        return r
-
-    @property
-    def average_prec(self, n_samples=11):
-        import numpy as np
-        pr = np.array(self.curve, np.float32)
-        return np.mean([np.max(pr[pr[:, 1] >= t, 0], initial=0) for t in np.linspace(0, 1, n_samples)])
-
-
 ## Helper functions ##
-def calculate_ap(model, data_loader, device):
-    pr_box = [PR() for _ in range(3)]
-    pr_dist = [PR(is_close=point_close) for _ in range(3)]
-    pr_iou = [PR(is_close=box_iou) for _ in range(3)]
-
-    for img, *gts in data_loader:
-        with torch.no_grad():
-
-            dets = model.detect(img.to(device))
-            for i, gt in enumerate(gts):
-                pr_box[i].add(dets[i], gt)
-                pr_dist[i].add(dets[i], gt)
-                pr_iou[i].add(dets[i], gt)
-
-        print("\nPR_box:", pr_box)
-        print("\nPR dist: ", pr_dist)
-        print("\nPR iou: ", pr_iou)
-
-        print("\t", "*" * 40, "\n")
-    return (np.mean(pr_box), np.mean(pr_dist), np.mean(pr_iou))
-
-
 def calculate_pos_weights(data_loader, device, q=0.66):
     # Initialize sums for calculating positive weights for each channel
     pos_sums = torch.zeros(3, device = device)  #3 heatmap channels
@@ -189,6 +85,8 @@ def train(args):
 
     # Set up data transformation
     transformation = dense_transforms.Compose([
+        dense_transforms.RandomHorizontalFlip(flip_prob = 0.5),
+        dense_transforms.ColorJitter(brightness = (0.7), contrast = (0.8), saturation = (0.7), hue = (0.2)),
         dense_transforms.ToTensor(),
         dense_transforms.ToHeatmap()
     ])
@@ -209,8 +107,8 @@ def train(args):
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
 
-    # Initialize best AP (average AP) value
-    best_ap = 0.0
+    # Initialize loss
+    current_loss = float(10000000.00)
     global_step = 0
 
     # Training loop
@@ -249,23 +147,20 @@ def train(args):
         model.eval()
 
         # Calculate AP values
-        if epoch % 3 == 0 or epoch == 0:
-            box, dist, iou = calculate_ap(model, valid_data, device)
-            average_ap = np.mean(box, dist, iou)
-            if average_ap > best_ap:
-                best_ap = average_ap
-                save_model(model)
-                print(f"Saved model with new best avg AP: {best_ap: .4f}")
-
      
+
+            
         # Update pos_weights
         updated_pos_weights = calculate_pos_weights(train_data, device)
         updated_pos_weights = updated_pos_weights.reshape(1, -1, 1, 1)
         heatmap_loss_function.pos_weight = updated_pos_weights.to(device)
-        #save_model(model)
-        print(f"Epoch {epoch+1}/{args.epochs}, Loss: {loss.item()}, New avg AP: {average_ap: .4f}, Updated Weights: {updated_pos_weights}")
-        #print(f"Epoch {epoch+1}/{args.epochs}, Loss: {loss.item()}, Updated Weights: {updated_pos_weights}")
+        print(f"Epoch {epoch+1}/{args.epochs}, Loss: {loss.item()}, Updated Weights: {updated_pos_weights}")
 
+        # Save model
+        if loss.item() < current_loss:
+            current_loss = loss.item
+            save_model(model)
+            print(f"Saving model at epoch {epoch+1} with loss of {loss.item()}")
 
 
 def log(logger, imgs, gt_det, det, global_step):
