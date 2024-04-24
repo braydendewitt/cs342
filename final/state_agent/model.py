618cfa3094e553b12a70b97f786279d0f5e8f820
braydendewitt <dewittbrayden@gmail.com>
2024-04-24 00:50:08 -0400

    submission 3, added (128,128) layer, 70L 63C

--- final/state_agent/model.py
+++ final/state_agent/model.py
@@ -8,12 +8,14 @@ class ImitationModel(nn.Module):
         super(ImitationModel, self).__init__()
         self.fc1 = nn.Linear(11, 64) # First layer of 11 features
         self.fc2 = nn.Linear(64, 128) # Hidden layer
-        self.fc3 = nn.Linear(128, 64) # Hidden layer
-        self.fc4 = nn.Linear(64, 3) # Output layer for 3 actions (accelerate, steer, brake)
+        self.fc3 = nn.Linear(128, 128)
+        self.fc4 = nn.Linear(128, 64) # Hidden layer
+        self.fc5 = nn.Linear(64, 3) # Output layer for 3 actions (accelerate, steer, brake)
 
     def forward(self, x):
         x = F.relu(self.fc1(x)) # Apply ReLU
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = self.fc4(x) # Get output
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x) # Get output
         return x
